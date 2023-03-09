30fc765e28d4d5f8ecff07a7cc80bc395e841be2
ARIEL NICOLLAS <39384032+arielnicollas@users.noreply.github.com>
2023-03-08 22:19:15 -0300

    feature: implementando titulo e sobre.

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,17 @@ function App() {
         </div>
       </div>
 
+      <div className='apresent-style'>
+        <h1>Prazer em conhecê-lo!</h1>
+        <h1>Eu sou Ariel Nicollas.</h1>
+        <p>Sou um desenvolvedor front-end apaixonado por criar aplicativos da web acessíveis 
+        <br></br>que os usuários adoram.
+        </p>
+      </div>
+
+      
+
+
     </div>
   );
 }
